64bacb16b9a93be739a8612729e5bdfb8283729c
jacemShady <guesmijacem8@gmail.com>
2026-05-03 23:18:53 +0100

    fixing app.component.spec.ts

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { ToastComponent } from './shared/components/toast.component';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
+    imports: [RouterTestingModule, ToastComponent],
+    declarations: [AppComponent],
   }));
 
   it('should create the app', () => {
